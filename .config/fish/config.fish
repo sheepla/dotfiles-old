01ca341bcdc758dcb297b902692d26227562025b
Sheepla <plusplus.sheep@gmail.com>
2021-05-22 18:45:10 +0900

    [add] fish config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+
+# 起動時のメッセージを無効に
+set fish_greeting
+
+# プロンプトを設定
+if which starship &>/dev/null
+    eval (starship init fish)
+end
+
+if which exa &>/dev/null
+    alias ls 'exa --classify --icons'
+    alias l  'exa -all --classify --icons'
+    alias ll 'exa -all --long --classify --icons'
+    alias lt 'exa -all --tree --classify --icons'
+end
+
+if which bat &>/dev/null
+    alias cat 'bat'
+end
+
+alias cls clear
+alias c clear
+
+if which ranger &>/dev/null
+    alias r 'ranger'
+end
+
+# fzf (jethrokuan/fzf)
+set -U FZF_LEGACY_KEYBINDINGS 0
+# set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=50%"
+
+if which enhancd &>/dev/null
+    alisa cd enhancd
+end
