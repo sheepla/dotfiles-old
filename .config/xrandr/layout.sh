6063a32502af09505094322276e6c6505b680581
sheepla <plusplus.sheep@gmail.com>
2021-06-13 20:53:39 +0900

    [update] xarndr/layout.sh

--- .config/xrandr/layout.sh
+++ .config/xrandr/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output Virtual1 --mode 1680x1050 --pos 0x0 --rotate normal --output Virtual2 --off --output Virtual3 --off --output Virtual4 --off --output Virtual5 --off --output Virtual6 --off --output Virtual7 --off --output Virtual8 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
